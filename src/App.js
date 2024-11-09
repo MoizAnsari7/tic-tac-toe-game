27858e4d7efa8eae4497bb9954802fa1972f3247
MoizAnsari7 <moizansari176@gmail.com>
2024-11-09 13:17:30 +0530

    basic dashboard designing, Game board component design, pass value of square box as props , game boxes default value setup via useState hook, change value when user click on box

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
-
+import GameArea from "./gameArea.component"
 function App() {
 return (<>
-  <div className="container">
+  <div className="container" style={{ marginTop : "11px" }}>
     <div className="header"></div>
     <div className="gameArea">
-      
+      Tic Tac Tae
     </div>
+      <GameArea />
   </div>
 </>)
 }
