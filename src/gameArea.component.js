51709b1e8c30ac0e2b820060b418661c98f5e614
MoizAnsari7 <moizansari176@gmail.com>
2024-11-09 14:23:21 +0530

    Winner logic implementation

--- src/gameArea.component.js
+++ src/gameArea.component.js
@@ -3,7 +3,21 @@ import Square from "./square.component"
 const GameAreaComponent = ()=>{
     const [ num , setNum ] = useState([ Array(9).fill(null) ])
     const [ turn , setTurn ] = useState( "X")
-    
+
+    const WinnerConditions = [
+        //verical
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        //horizontal
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        //diagonal
+        [0,4,8],
+        [2,4,6],
+    ]
+
     console.log(num);
     const handleClick = (squareIndex )=>{
         console.log(`clicked square index is ${ squareIndex }`)
@@ -12,7 +26,27 @@ const GameAreaComponent = ()=>{
         //Set value of current turn player in array
         newArr[ squareIndex ] = turn;
         setNum( newArr )
+        if(!checkWinner())
+        {
         setTurn( ( turn === "X" ) ? "O" : "X" )
+        }
+        //check winner
+        
+    }
+
+    const checkWinner = ()=>{
+        for(let i = 0; i < WinnerConditions.length; i++)
+        {
+            console.log(  WinnerConditions[i], num[WinnerConditions[i][0]], num[WinnerConditions[i][1]], num[WinnerConditions[i][2]])
+            if( num[WinnerConditions[i][0]]  != null &&   num[WinnerConditions[i][1]]  != null &&  num[WinnerConditions[i][2]]  != null)
+            {
+                console.log("matched", WinnerConditions[i]);
+                console.log(`Player ${ num[WinnerConditions[i][0]] } is winner`);
+                return true;     
+            }else{
+                return false;
+            }
+        }
     }
 
     const handleReset = ()=>{
@@ -23,8 +57,8 @@ const GameAreaComponent = ()=>{
             <div >
                 <button onClick={handleReset}>Reset Game</button>
                 <h2> { turn } : Your Turn </h2>
-                <div style={{  display : "flex", justifyContent: "cnenter", alignItems : "center" }} className="row">
-                    <Square value={num[0]} selectHandle={ ()=>{ handleClick(0)}}/>
+                <div className="row">
+                    <Square value={num[0]} selectHandle={ ()=>{ handleClick(0)}} />
                     <Square value={num[1]} selectHandle={ ()=>{ handleClick(1)}} />
                     <Square value={num[2]} selectHandle={ ()=>{ handleClick(2)}} />
                 </div>
--- src/square.component.js
+++ src/square.component.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-const squareComponent = (props)=>{
-    return (
-        <div onClick={ props.selectHandle } style={ { display:"flex", width: "180px", height: "180px", justifyContent: "center", alignItems: "center",  border : "2px solid black"} }><p style={{  fontSize : "110px"}}>{props.value}</p></div>
-    )
-}
+const squareComponent = (props)=>(
+    <div onClick={ props.selectHandle } className="box"><p style={{  fontSize : "110px"}}>{props.value}</p></div>
+    // <div className="box"><p style={{ fontSize: "110px" }}>{props.value}</p></div>
+    // <div  className="box"><p style={{  fontSize : "110px"}}>X</p></div>
+
+)
 
 export default squareComponent;
