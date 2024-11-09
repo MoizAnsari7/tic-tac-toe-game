5aec662102c4d8e6c7aeafbcfe0b9433cc5719ad
MoizAnsari7 <moizansari176@gmail.com>
2024-11-09 13:27:24 +0530

    functionality implemented for player turn. if tuen is 'X' player then fill selected box with 'X' and flip turn for 'O'

--- src/gameArea.component.js
+++ src/gameArea.component.js
@@ -2,17 +2,22 @@ import React, {  useState } from "react";
 import Square from "./square.component"
 const GameAreaComponent = ()=>{
     const [ num , setNum ] = useState([ Array(9).fill(null) ])
+    const [ turn , setTurn ] = useState( "X")
+    
     console.log(num);
     const handleClick = (squareIndex )=>{
         console.log(`clicked square index is ${ squareIndex }`)
         //change square box value according to player turn.
         let newArr = [ ...num ];
-        newArr[ squareIndex ] = "X";
+        //Set value of current turn player in array
+        newArr[ squareIndex ] = turn;
         setNum( newArr )
+        setTurn( ( turn === "X" ) ? "O" : "X" )
     }
     return (
         <>
             <div >
+                <h2> { turn } : Your Turn </h2>
                 <div style={{  display : "flex", justifyContent: "cnenter", alignItems : "center" }} className="row">
                     <Square value={num[0]} selectHandle={ ()=>{ handleClick(0)}}/>
                     <Square value={num[1]} selectHandle={ ()=>{ handleClick(1)}} />
